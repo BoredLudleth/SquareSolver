186ff8a2fe4a86498243c2ba63f4015a72acf461
Ludlet <111277087+Ludlet@users.noreply.github.com>
2022-08-15 21:32:55 +0300

    v.2.0
    1)Every variable inicialized
    2)Each logical block placed in function
    3)Useless coments were deleted

--- SquareEquation.cpp
+++ SquareEquation.cpp
@@ -1,41 +1,96 @@
 #include <stdio.h>
-#include <math.h>                                 //uses for pow()
+#include <math.h>                                 //uses for pow() and for NAN
 
 int main(void)
 {
-    double a, b, c;                               //equality odds a*x^2 + b*x + c = 0
-    double dis;                                   //discriminant
+    double a = NAN, b = NAN, c = NAN;
+    void greeting();
+    int input(double &k1, double &k2, double &k3);
+    void solve(double k1, double k2, double k3);
+    void goodbye();
 
+    greeting();
+    while (input(a, b, c))
+        solve(a, b, c);
+    goodbye();
+
+}
+
+void greeting()
+{
     printf("Hello, this program helps to solve quadratic equations!\n");
     printf("Type any symbol to exit\n");
+}
+
+int input(double &k1, double &k2, double &k3)
+{
     printf("Enter the coefficients a, b and c separated by a space\n");
+    if (scanf("%lf %lf %lf", &k1, &k2, &k3) == 3)
+        return 1;
+    else
+        return 0;
+}
 
-    while (scanf("%lf %lf %lf", &a, &b, &c)) {
-        //case, when it's linear equation
-        if (a == 0) {                             //case, when a = 0: b*x + c = 0(infinity or 1 or 0 solves)
-            if (b == 0) {                         //case, when a = b = 0: c = 0
-                if (c == 0)                       //case, when a = b = c = 0: 0 = 0
-                    printf("An infinite number of solutions\n");
-                else
-                    printf("No solutions\n");
-            } else
-                printf("One solution x=%lf\n", -c/b);
+void solve(double k1, double k2, double k3)
+{
+    void infinitySolution();
+    void noSolution();
+    void linearSolution(double k2, double k3);
+    void oneSquareSolution(double k1, double k2);
+    void twoSquareSolution(double k1, double k2, double k3);
+    double dis = NAN;
 
-        //case, when it's quadratic equation(2 or 1 or 0 solves)
-        } else {
-            dis = b*b - 4*a*c;
-            if (dis > 0)
-                printf("Two solutions x1 = %lf, x2 = %lf\n",
-(-b - pow(dis, 0.5))/(2*a), (-b + pow(dis, 0.5))/(2*a));
-            else if (dis == 0)
-                printf("One solution x = %lf\n", -b/(2*a));
+    //case, when it's linear equation
+    if (k1 == 0) {
+        if (k2 == 0) {
+            if (k3 == 0)
+                infinitySolution();
             else
-                printf("No solutions\n");
-        }
-        printf("Enter the coefficients a, b and c separated by a space\n");
-
+                noSolution();
+        } else
+            linearSolution(k2, k3);
+    //case, when it's quadratic equation
+    } else {
+        dis = k2*k2 - 4*k1*k3;
+        if (dis > 0)
+            twoSquareSolution(k1, k2, k3);
+        else if (dis == 0)
+            oneSquareSolution(k1, k2);
+        else
+            noSolution();
     }
+}
 
+void infinitySolution()
+{
+    printf("An infinite number of solutions\n");
+}
+
+void noSolution()
+{
+    printf("No solutions\n");
+}
+
+void linearSolution(double k2, double k3)
+{
+    printf("One solution x=%lf\n", -k3/k2);
+}
+
+void oneSquareSolution(double k1, double k2)
+{
+    printf("One solution x = %lf\n", -k2/(2*k1));
+}
+
+void twoSquareSolution(double k1, double k2, double k3)
+{
+    double dis = NAN;
+
+    dis = k2*k2 - 4*k1*k3;
+    printf("Two solutions x1 = %lf, x2 = %lf\n",
+    (-k2 - pow(dis, 0.5))/(2*k1), (-k2 + pow(dis, 0.5))/(2*k1));
+}
+
+void goodbye()
+{
     printf("Program completed! Have a good day!\n");
-
 }
