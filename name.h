5d11014f2ceb8664a3f963a7c75664e14f01b6d1
Ludlet <111277087+Ludlet@users.noreply.github.com>
2022-08-16 16:55:16 +0300

    Update name.h

--- name.h
+++ name.h
@@ -1,5 +1,5 @@
 void greeting();
-int input(double &k1, double &k2, double &k3);
+int input(double *k1, double *k2, double *k3);
 void solve(double k1, double k2, double k3, double &s1, double &s2);
 void output(double s1, double s2);
 void infinitySolution();
