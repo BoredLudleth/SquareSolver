256438d380eb2890caa7521f8ea64923be6bf450
Ludlet <111277087+Ludlet@users.noreply.github.com>
2022-08-16 21:56:32 +0300

    Update SquareEquation.cpp

--- SquareEquation.cpp
+++ SquareEquation.cpp
@@ -2,13 +2,13 @@
 #include <math.h>                                 //uses for pow() and for NAN and isnan()
 #include "name.h"
 
-int main(void)
+int main()
 {
     double a = NAN, b = NAN, c = NAN, x1 = NAN, x2 = NAN;
 
     greeting();
     while(input(&a, &b, &c)) {
-        solve(a, b, c, x1, x2);
+        solve(a, b, c, &x1, &x2);
         output(x1, x2);
     }
     goodbye();
@@ -31,29 +31,29 @@ int input(double *k1, double *k2, double *k3)
         return 0;
 }
 
-void solve(double k1, double k2, double k3, double &s1, double &s2)
+void solve(double k1, double k2, double k3, double *s1, double *s2)
 {
     double dis = NAN;
 
-    s1 = s2 = NAN;
+    *s1 = *s2 = NAN;
     //case, when it's linear equation
     if (k1 == 0) {
         if (k2 == 0) {
             if (k3 == 0) {
-                s1 = 0;
-                s2 = 0;
+                *s1 = 0;
+                *s2 = 0;
             }
         } else
-            s1 = -k3/k2;
+            *s1 = -k3/k2;
     //case, when it's quadratic equation
     } else {
         dis = k2*k2 - 4*k1*k3;
         if (dis > 0) {
-            s1 = (-k2 - pow(dis, 0.5))/(2*k1);
-            s2 = (-k2 + pow(dis, 0.5))/(2*k1);
+            *s1 = (-k2 - pow(dis, 0.5))/(2*k1);
+            *s2 = (-k2 + pow(dis, 0.5))/(2*k1);
         }
         else if (dis == 0)
-            s1 = -k2/(2*k1);
+            *s1 = -k2/(2*k1);
     }
 }
 
