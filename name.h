06b9a49196cee87916b074443df26c5e0488bbde
Ludlet <111277087+Ludlet@users.noreply.github.com>
2022-08-16 10:55:33 +0300

    Add files via upload

---
+++ name.h
@@ -0,0 +1,9 @@
+void greeting();
+int input(double &k1, double &k2, double &k3);
+void solve(double k1, double k2, double k3, double &s1, double &s2);
+void output(double s1, double s2);
+void infinitySolution();
+void noSolution();
+void oneSolution(double s1);
+void twoSquareSolution(double s1, double s2);
+void goodbye();
