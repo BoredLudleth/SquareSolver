3a744840703bbf320b063b9a404ccd3c093f03d1
Ludlet <111277087+Ludlet@users.noreply.github.com>
2022-08-17 08:23:38 +0300

    Update SquareEquation.cpp

--- SquareEquation.cpp
+++ SquareEquation.cpp
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <math.h>                                 //uses for pow() and for NAN and isnan()
+#include <math.h>                                 //uses for pow() and for NAN
 #include "name.h"
 
 int main()
 {
     double a = NAN, b = NAN, c = NAN, x1 = NAN, x2 = NAN;
+    int x_count = -1;
 
     greeting();
     while(input(&a, &b, &c)) {
-        solve(a, b, c, &x1, &x2);
-        output(x1, x2);
+        solve(a, b, c, &x1, &x2, &x_count);
+        output(x_count, x1, x2);
     }
     goodbye();
 
@@ -31,63 +32,56 @@ int input(double *k1, double *k2, double *k3)
         return 0;
 }
 
-void solve(double k1, double k2, double k3, double *s1, double *s2)
+void solve(double k1, double k2, double k3, double *s1, double *s2, int *roots)
 {
     double dis = NAN;
 
     *s1 = *s2 = NAN;
+    *roots = -1;
     //case, when it's linear equation
     if (k1 == 0) {
         if (k2 == 0) {
             if (k3 == 0) {
-                *s1 = 0;
-                *s2 = 0;
-            }
-        } else
+                *roots = 3;
+            } else
+                *roots = 0;
+        } else {
             *s1 = -k3/k2;
+            *roots = 1;
+        }
     //case, when it's quadratic equation
     } else {
         dis = k2*k2 - 4*k1*k3;
         if (dis > 0) {
             *s1 = (-k2 - pow(dis, 0.5))/(2*k1);
             *s2 = (-k2 + pow(dis, 0.5))/(2*k1);
+            *roots = 2;
         }
-        else if (dis == 0)
+        else if (dis == 0) {
             *s1 = -k2/(2*k1);
+            *roots = 1;
+        } else
+            *roots = 0;
     }
 }
 
-void output(double s1, double s2)
+void output(int roots, double s1, double s2)
 {
-    if (s1 == 0 and s2 == 0)
-        infinitySolution();
-    else if (!isnan(s1) and !isnan(s2))
-        twoSquareSolution(s1, s2);
-    else if (!isnan(s1) and isnan(s2))
-        oneSolution(s1);
-    else if (isnan(s1) and isnan(s2))
-        noSolution();
+    switch(roots) {
+        case 0:
+            printf("No solutions\n");
+            break;
+        case 1:
+            printf("One solution x = %lf\n", s1);
+            break;
+        case 2:
+            printf("Two solutions x1 = %lf, x2 = %lf\n", s1, s2);
+            break;
+        case 3:
+            printf("An infinite number of solutions\n");
+            break;
+    }
 
-}
-
-void infinitySolution()
-{
-    printf("An infinite number of solutions\n");
-}
-
-void noSolution()
-{
-    printf("No solutions\n");
-}
-
-void oneSolution(double s1)
-{
-    printf("One solution x = %lf\n", s1);
-}
-
-void twoSquareSolution(double s1, double s2)
-{
-    printf("Two solutions x1 = %lf, x2 = %lf\n", s1, s2);
 }
 
 void goodbye()
