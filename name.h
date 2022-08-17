a4e180be07853ef3e4d643467773f1a045c49b2c
Ludlet <111277087+Ludlet@users.noreply.github.com>
2022-08-17 22:14:53 +0300

    Update name.h

--- name.h
+++ name.h
@@ -1,6 +1,7 @@
 void greeting();
 int input(double *k1, double *k2, double *k3);
-void solve(double k1, double k2, double k3, double *s1, double *s2, int *roots);
+int solve(double k1, double k2, double k3, double *s1, double *s2);
+int linearSolve(double k2, double k3, double *s1);
+int squareSolve(double k1, double k2, double k3, double *s1, double *s2);
 void output(int roots, double s1, double s2);
 void goodbye();
-
