bc8799535bcefd3b71de57e25e64b6f49a46e983
Ludlet <111277087+Ludlet@users.noreply.github.com>
2022-08-18 14:12:25 +0300

    Update and rename name.h to SquareSolver.h

--- SquareSolver.h
+++ SquareSolver.h
@@ -1,10 +1,19 @@
 void greeting();
+
 int input(double *k1, double *k2, double *k3);
+
 void failedInput();
+
 int isequal(double x, double y);
+
 int solve(double k1, double k2, double k3, double *s1, double *s2);
+
 int linearSolve(double k2, double k3, double *s1);
+
 int squareSolve(double k1, double k2, double k3, double *s1, double *s2);
-void output(int roots, double s1, double s2);
+
+void output(double s1, double s2, int ans);
+
 void nextEquation(char *sym);
+
 void goodbye();
