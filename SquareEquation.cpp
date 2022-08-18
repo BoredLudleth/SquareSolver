2d4281ad7ef26aa16f0da25b1b532c44028c676e
Ludlet <111277087+Ludlet@users.noreply.github.com>
2022-08-18 11:20:16 +0300

    Update SquareEquation.cpp

--- SquareEquation.cpp
+++ SquareEquation.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>                                 //uses for pow() and for NAN
+#include <assert.h>
+#include <ctype.h>
 #include "name.h"
 #include "deffunc.cpp"
 
@@ -7,11 +9,15 @@ int main()
 {
     double a = NAN, b = NAN, c = NAN, x1 = NAN, x2 = NAN;
     int x_count = -1;
+    char cont = 'y';
 
     greeting();
-    while(input(&a, &b, &c)) {
+    while (cont == 'y'){
+        while(!input(&a, &b, &c))
+            failedInput();
         x_count = solve(a, b, c, &x1, &x2);
         output(x_count, x1, x2);
+        nextEquation(&cont);
     }
     goodbye();
 
