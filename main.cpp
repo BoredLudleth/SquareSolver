3921f9d86c0225b2aa2622463871c7c15a0dc4f8
Ludlet <111277087+Ludlet@users.noreply.github.com>
2022-08-19 16:33:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@
 #include "SquareSolver.hpp"
 
 int main() {
-    double a = NAN, b = NAN, c = NAN, x1 = NAN, x2 = NAN;
     char cont = 'y';
-    enum allAnswers ans;
 
     greeting();
-    while (cont == 'y') {
+    while(cont == 'y') {
+        double a = NAN, b = NAN, c = NAN, x1 = NAN, x2 = NAN;
+
         while(!input(&a, &b, &c)) failedInput();
+        enum allAnswers ans = allAnswers(0);
         ans = (allAnswers) solve(a, b, c, &x1, &x2);
         output(x1, x2, ans);
         nextEquation(&cont);
