e61c180361986ff1d9420b0f68c985231b46e646
BoredLudleth <111277087+BoredLudleth@users.noreply.github.com>
2022-08-24 16:32:12 +0300

    Update Tests.cpp

--- Tests.cpp
+++ Tests.cpp
@@ -8,22 +8,29 @@
 #include "SquareSolver.hpp"
 #include "Tests.hpp"
 
-void alltests() {
+int main() {
     double a = NAN, b = NAN, c = NAN;
     enum allAnswers ans;
-    FILE *mytests;
 
-    mytests = fopen("TEST.txt", "r");
-    while(fscanf(mytests, "%lf %lf %lf %d", &a, &b, &c, &ans) == 4)
+    FILE *mytests = fopen("TEST.txt", "r");
+    if (mytests == NULL) {
+        printf("Error, can't find file TEST.txt");
+        return 1;
+    }
+
+    while (fscanf(mytests, "%lf %lf %lf %d", &a, &b, &c, &ans) == 4) {
        test(a, b, c, ans);
+    }
+
     fclose(mytests);
+    return 0;
 }
 
 void test(double k1, double k2, double k3, int correctAns) {
     double x1 = NAN, x2 = NAN;
     bool is_correct = false;
 
-    if (solve(k1, k2, k3, &x1, &x2) == correctAns) {
+    if (squareSolve(k1, k2, k3, &x1, &x2) == correctAns) {
         if (correctAns == INFINITYANSWERS or correctAns == NOANSWERS) {
             is_correct = true;
         } else if (correctAns == ONEANSWER) {
