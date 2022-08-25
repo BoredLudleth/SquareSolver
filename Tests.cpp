07541aab454a43791fbe1ed16bd35e0d7d9ab018
BoredLudleth <111277087+BoredLudleth@users.noreply.github.com>
2022-08-26 00:55:42 +0300

    Update Tests.cpp

--- Tests.cpp
+++ Tests.cpp
@@ -10,40 +10,47 @@
 
 int main() {
     double a = NAN, b = NAN, c = NAN;
+    int count_wrong = 0;
     enum allAnswers ans;
 
     FILE *mytests = fopen("TEST.txt", "r");
     if (mytests == NULL) {
-        printf("Error, can't find file TEST.txt");
+        printf("Error, can't find file TEST.txt\n");
         return 1;
     }
 
+    int num_test = 1;
     while (fscanf(mytests, "%lf %lf %lf %d", &a, &b, &c, &ans) == 4) {
-       test(a, b, c, ans);
+       count_wrong += test(a, b, c, ans, num_test);
+       num_test++;
     }
+    printf("Wrong tests - %d\n", count_wrong);
 
     fclose(mytests);
     return 0;
 }
 
-void test(double k1, double k2, double k3, int correctAns) {
+int test(double a, double b, double c, int correctAns, int num_test) {
     double x1 = NAN, x2 = NAN;
     bool is_correct = false;
 
-    if (squareSolve(k1, k2, k3, &x1, &x2) == correctAns) {
+    if (squareSolve(a, b, c, &x1, &x2) == correctAns) {
         if (correctAns == INFINITYANSWERS or correctAns == NOANSWERS) {
             is_correct = true;
         } else if (correctAns == ONEANSWER) {
-            if (isequal(k1 * x1 * x1 + k2 * x1 + k3, 0))
+            if (isEqual(a * x1 * x1 + b * x1 + c, 0))
                 is_correct = true;
         } else if (correctAns == TWOANSWERS) {
-            if (isequal(k1 * x1 * x1 + k2 * x1 + k3, 0) && isequal(k1 * x2 * x2 + k2 * x2 + k3, 0))
+            if (isEqual(a * x1 * x1 + b * x1 + c, 0) && isEqual(a * x2 * x2 + b * x2 + c, 0))
                 is_correct = true;
         }
     }
-    if (is_correct)
-        printf("Test[#] - True\n");
-    else
-        printf("Test[#] - False. Problem with a = %lf, b = %lf, c = %lf,"
-               " found roots x1 = %lf, x2 = %lf\n", k1, k2, k3, x1, x2);
+    if (is_correct) {
+        printf("Test[#%d] - True\n", num_test);
+        return 0;
+    } else {
+        printf("Test[#%d] - False. Problem with a = %lf, b = %lf, c = %lf, "
+               "found roots x1 = %lf, x2 = %lf\n", num_test, a, b, c, x1, x2);
+        return 1;
+    }
 }
